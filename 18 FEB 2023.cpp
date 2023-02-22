be73819def28245da2c77030eed899a477994d28
HARSH289CHAUDHARY <114799920+HARSH289CHAUDHARY@users.noreply.github.com>
2023-02-22 06:56:42 +0530

    Add files via upload

---
+++ 18 FEB 2023.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/*void main(){
+	int a,i;
+	int b = 1;
+	scanf("%d",&a);
+	for (i=1;i<=a;i++){
+		b = b*i;
+	}
+	printf("%d",b);*/
+	
+	
+	
+	
+	
+	void main(){
+	int i = 10;
+	do{
+		printf("%d",i);
+	}
+	while (i<0);
+}
